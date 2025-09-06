3a892937fabc825609ff2f9e41c8117da4edc31c
JHK0723 <jhk.300506@gmail.com>
2025-09-07 02:46:25 +0530

    only one Doctor class for google auth user validation

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,16 +2,11 @@ from pydantic import BaseModel, HttpUrl
 from typing import Optional
 
 # Used when a user signs in (Google sends profile data)
-class UserCreate(BaseModel):
-    google_id: str
-    name: str
-    picture: Optional[HttpUrl] = None   # use HttpUrl to validate proper image link
-
-# Used when returning user data from the DB
-class UserResponse(BaseModel):
-    id: int
+class Doctor(BaseModel):
+    id: Optional[int] = None  #optional because DB fills it
     google_id: str
     name: str
     picture: Optional[str] = None
+
     class Config:
         orm_mode = True
