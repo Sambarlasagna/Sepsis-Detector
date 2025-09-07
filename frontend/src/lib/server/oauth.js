166e94ae04f33a2424ca376b9ec99a3b2030f851
Sulaimanibrahim06 <sulaimanibrahim3108@gmail.com>
2025-09-07 14:34:32 +0530

    ...

--- frontend/src/lib/server/oauth.js
+++ frontend/src/lib/server/oauth.js
@@ -3,6 +3,6 @@ import { Google } from "arctic";
 export const google = new Google(
 	process.env.GOOGLE_CLIENT_ID ?? "",
 	process.env.GOOGLE_CLIENT_SECRET ?? "",
-	"https://sepsis-sense/login/google/callback",
+	"https://sepsis-sense.vercel.app/login/google/callback",
   "http://localhost:3000/login/google/callback" 
 );
